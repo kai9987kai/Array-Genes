be490ba182549f2d269634c1411136633f8e0ac3
Kai Piper <kai9987kai@gmail.com>
2023-05-31 20:51:10 +0100

    Update Base model.py

--- Base model.py
+++ Base model.py
@@ -29,6 +29,13 @@ GENETIC_ATTRIBUTES = {
         "C": True,
         "D": False,
         "E": True
+    },
+    "max_age": {
+        "A": 60,
+        "B": 70,
+        "C": 80,
+        "D": 90,
+        "E": 100
     }
 }
 
@@ -45,6 +52,7 @@ class Bot(turtle.Turtle):
         self.genetic_code = genetic_code
         self.speed_gene = GENETIC_ATTRIBUTES["speed"][self.genetic_code[0]]
         self.hostility = GENETIC_ATTRIBUTES["hostility"][self.genetic_code[1]]
+        self.max_age = GENETIC_ATTRIBUTES["max_age"][self.genetic_code[2]]
         self.age = age
 
     def move(self):
@@ -54,11 +62,18 @@ class Bot(turtle.Turtle):
         if self.moves == 4 and not self.hostility:
             self.reproduce()
             self.moves = 0
+        if self.age > self.max_age:  # If bot is too old, it dies
+            bots.remove(self)
+            self.hideturtle()
+            del self
 
     def reproduce(self):
         x = random.randint(-300, 300)
         y = random.randint(-200, 200)
         child_genetic_code = self.genetic_code + random.choice('ABCDE')
+        if random.random() < 0.01:  # 1% chance of mutation
+            mutation_index = random.randint(0, len(child_genetic_code) - 1)
+            child_genetic_code = child_genetic_code[:mutation_index] + random.choice('ABCDE') + child_genetic_code[mutation_index + 1:]
         child = Bot(x, y, self.color()[0], child_genetic_code)
         bots.append(child)
 
@@ -68,6 +83,11 @@ class Bot(turtle.Turtle):
             plant.hideturtle()
             del plant
             self.moves = 0
+            # Spawn a new plant at a random location
+            x = random.randint(-300, 300)
+            y = random.randint(-200, 200)
+            new_plant = Plant(x, y)
+            plants.append(new_plant)
 
     def fight(self, other):
         if self.hostility and self.distance(other) < 20:
@@ -87,6 +107,7 @@ class Plant(turtle.Turtle):
         self.penup()
         self.goto(x, y)
         self.color('green')
+
         self.shape('circle')
 
 # Obstacle class
@@ -103,14 +124,13 @@ for _ in range(10):
     x = random.randint(-300, 300)
     y = random.randint(-200, 200)
     color = random.choice(['blue', 'pink'])
-    genetic_code = random.choice('ABCDE') + random.choice('ABCDE')
+    genetic_code = random.choice('ABCDE') + random.choice('ABCDE') + random.choice('ABCDE')
     bot = Bot(x, y, color, genetic_code)
     bots.append(bot)
 
 # Create initial plants
 for _ in range(20):
     x = random.randint(-300, 300)
-
     y = random.randint(-200, 200)
     plant = Plant(x, y)
     plants.append(plant)
@@ -134,10 +154,6 @@ while True:
                 bot.fight(other)
         if bot.xcor() > 390 or bot.xcor() < -390 or bot.ycor() > 290 or bot.ycor() < -290:
             bot.right(180)
-        if bot.age > 100:  # If bot is too old, it dies
-            bots.remove(bot)
-            bot.hideturtle()
-            del bot
         bot.display_stats()  # Display bot's stats
 
 turtle.done()
